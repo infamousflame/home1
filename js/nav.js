c4bb4ce68435ad40ebfd90a10757acfb5d453a58
infamousflame <Owenlevin@yahoo.com>
2016-10-28 03:30:54 +0000

    almost working nav

--- js/nav.js
+++ js/nav.js
@@ -1,27 +1,53 @@
 var list = $("#globalNav>ul");
+var listEl = $("#globalNav>ul li");
 var logoEl = $("#globalNav>.logo");
 
 function listLeft(){
     list.css("position", "fixed");
-    list.css("color", "white")
-    list.css("left", "-100px");
-    list.css("z-index", "999")
+    list.css("width", "200px");
+    list.css("height", "100%");
+    list.css("color", "black");
+    list.css("left", "-200px");
+    list.css("z-index", "999");
+    list.css("margin-top", "120px");
+    list.css("list-style-type", "none");
+    list.css("background-color", "rgba(255,255,255,.8)");
+    list.css("filter", "(4px)");
+    listEl.css("margin-top", "50px");
 }
 
 function logo(){
     logoEl.css("width", "100px");
     logoEl.css("position", "fixed");
     logoEl.css("cursor","pointer");
-    logoEl.css("z-index","999");
-    
+    logoEl.css("z-index","9999");
 }
 
 function goRight(){
-    alert("yes");
+    list.animate({
+        left: "+=200px"
+    }, 200);
+}
+
+function goLeft(){
+    list.animate({
+        left: "-=200px"
+    }, 200);
+}
+
+function toggleNav(){
+    if(list.hasClass("toggled")){
+        goLeft();
+        list.removeClass("toggled");
+    }else{
+        goRight();
+        list.addClass("toggled");
+        console.log("xd");
+    }
 }
 
 
 listLeft();
 logo();
 
-logoEl.on("click", goRight);
+logoEl.on("click", toggleNav);
